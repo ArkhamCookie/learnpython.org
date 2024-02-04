2cfa09e12d267d594bab002d7b39410a958f264f
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-04 13:22:31 -0600

    Complete variables & types section

---
+++ variables+types/script.py
@@ -0,0 +1,24 @@
+# Numebrs
+myInt = 7
+print(myInt)
+
+myFloat = 7.0
+print(myFloat)
+
+# Strings
+myString = 'hello'
+print(myString)
+
+myString = "Don't worry about apostrophes!"
+print(myString)
+
+# Combining Strings
+hello = 'Hello'
+world = 'Mom'
+helloWorld = hello + ', ' + world + '!'
+
+print(helloWorld)
+
+# Assign more than one var
+a, b = 3, 4
+print(a, b)
