40b2d2f0581f8b6baa85005b205f3a11532a1292
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-04 13:44:07 -0600

    Completed list exercise

---
+++ lists/script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+myList = []
+
+myList.append(1)
+myList.append(2)
+myList.append(3)
+
+print(myList[0], myList[1], myList[2])
+
+for x in myList:
+    print(x)
+
+# Exercise
+names = ["John", "Eric", "Jessica"]
+
+print("The second name on the names list is %s." % names[1])
