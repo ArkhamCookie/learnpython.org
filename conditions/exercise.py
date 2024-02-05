8e7084bf383e81c73d8e5dfc4769561a3e31e89a
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-05 12:43:30 -0600

    Complete contitions section

---
+++ conditions/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+number = 16
+number2 = 0
+firstArray = [1,2,3]
+secondArray = [1,2]
+
+if number > 15:
+    print('1')
+
+if firstArray:
+    print('2')
+
+if len(secondArray) == 2:
+    print('3')
+
+if len(firstArray) + len(secondArray) == 5:
+    print('4')
+
+if firstArray and firstArray[0] == 1:
+    print('5')
+
+if not number2:
+    print('6')
